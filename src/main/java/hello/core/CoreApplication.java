83325f082656c919ed07d54651effe40335489af
kimmedev <80742971+kimmekimme@users.noreply.github.com>
2022-01-24 09:20:55 +0900

    Initial commit

---
+++ src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Console;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
