9be0ee201401ef255067c2e2438a98b54ae9202b
kimmedev <80742971+kimmekimme@users.noreply.github.com>
2022-01-27 17:58:44 +0900

    스프링 컨테이너 생성, 빈 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 //ctrl + E: 최근 파일
 //기획자: 역할과 구현 분리
+//IOC컨테이너(DI컨테이너)
+//GO to Class via Ctrl + N
+
+@Configuration //설정구성정보 어노테이션
 public class AppConfig {
 //마우스 커서 갖다대고 f4: 소스 편집
 //ctrl + art + m: 메소드 추출
+
+    @Bean//(name = "이름") 지정가능
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
         //return new FixDiscountPolicy();
     }
---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,42 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //iter + tab: 리스트나 배열이 있으면 for문 자동완성
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " Object = " + bean); //sout + v: 변수명 찍어주는 단축키
+            //sout + m: 메소드명 찍어주는 단축키
+        }
+}
+//ctrl + d: 밑에복사
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //iter + tab: 리스트나 배열이 있으면 for문 자동완성
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //내가 직접 등록한 빈 조회: ROLE_APPLICATION
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) { //ROLE_INFRASTRUCTURE: 스프링 내부사용 빈
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " Object = " + bean); //sout + v: 변수명 찍어주는 단축키
+            }
+
+            //sout + m: 메소드명 찍어주는 단축키
+        }
+    }
+}
