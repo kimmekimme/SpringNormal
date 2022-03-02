6d6a695b5e401b681cf35ea6861cf59b49adbd1e
kimmedev <80742971+kimmekimme@users.noreply.github.com>
2022-03-02 17:39:19 +0900

    lombok: @RequiredArgsConstructor, @Getter, @Setter Bean: Prototype, Singleton

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,10 @@
 package hello.core.discount;
 
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
+//@("FixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -12,13 +13,13 @@ import org.springframework.stereotype.Component;
 import org.w3c.dom.Node;
 
 @Component
-@RequiredArgsConstructor //final이 붙은 필드로 생성자를 만들어줌
+//@RequiredArgsConstructor //final�씠 遺숈� �븘�뱶濡� �깮�꽦�옄瑜� 留뚮뱾�뼱以�
 public class OrderServiceImpl implements OrderService {
 
     //
-    private final MemberRepository memberRepository;//회원찾기
-    private final DiscountPolicy discountPolicy;//널포인트익셉션 어떻게?
-
+    private final MemberRepository memberRepository;//�쉶�썝李얘린
+    private final DiscountPolicy discountPolicy;//�꼸�룷�씤�듃�씡�뀎�뀡 �뼱�뼸寃�?
+    
 //    @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        //System.out.println("discountPolicy = " + discountPolicy);
@@ -30,20 +31,20 @@ public class OrderServiceImpl implements OrderService {
 //        //System.out.println("memberRepository = " + memberRepository);
 //        this.memberRepository = memberRepository;
 //    }
-//생성자 없어도 되고, 생성자 주입이 가장 먼저 호출 후 수정자 주입 호출
-    // @Autowired //의존관계 자동 주입, 생성자 하나면 생략가능
+//�깮�꽦�옄 �뾾�뼱�룄 �릺怨�, �깮�꽦�옄 二쇱엯�씠 媛��옣 癒쇱� �샇異� �썑 �닔�젙�옄 二쇱엯 �샇異�
+    // @Autowired //�쓽議닿�怨� �옄�룞 二쇱엯, �깮�꽦�옄 �븯�굹硫� �깮�왂媛��뒫
 
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
 
-
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //할인찾기
-    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP 위반
+
+
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //�븷�씤李얘린
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP �쐞諛�
 
 
 
@@ -51,12 +52,12 @@ public class OrderServiceImpl implements OrderService {
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-        //ctrl alt v 추출
+        //ctrl alt v 異붿텧
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    //테스트 용도
+    //�뀒�뒪�듃 �슜�룄
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
